993e464e4395286f153bb6cceb46ef5155840b6a
AHMADSOULA <Ahmadmom799@gmail.com>
2026-09-24 17:21:27 +0100

    Create stealth.py

---
+++ automation/stealth.py
@@ -0,0 +1,63 @@
+STEALTH_JS = r"""
+Object.defineProperty(navigator, 'webdriver', { get: () => undefined });
+delete Object.getPrototypeOf(navigator).webdriver;
+Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] });
+Object.defineProperty(navigator, 'platform', { get: () => 'Win32' });
+Object.defineProperty(navigator, 'hardwareConcurrency', { get: () => 8 });
+Object.defineProperty(navigator, 'deviceMemory', { get: () => 8 });
+Object.defineProperty(navigator, 'maxTouchPoints', { get: () => 0 });
+Object.defineProperty(navigator, 'plugins', {
+    get: () => {
+        const plugins = [
+            { name: 'Chrome PDF Plugin', filename: 'internal-pdf-viewer' },
+            { name: 'Chrome PDF Viewer', filename: 'mhjfbmdgcfjbbpaeojofohoefgiehjai' },
+            { name: 'Native Client', filename: 'internal-nacl-plugin' },
+        ];
+        plugins.refresh = () => {};
+        plugins.item = (i) => plugins[i];
+        plugins.namedItem = (n) => plugins.find(p => p.name === n);
+        return plugins;
+    }
+});
+Object.defineProperty(navigator, 'mimeTypes', {
+    get: () => [{ type: 'application/pdf' }, { type: 'text/pdf' }]
+});
+const getParameter = WebGLRenderingContext.prototype.getParameter;
+WebGLRenderingContext.prototype.getParameter = function(parameter) {
+    if (parameter === 37445) return 'Intel Inc.';
+    if (parameter === 37446) return 'Intel Iris OpenGL Engine';
+    return getParameter.call(this, parameter);
+};
+const getParameter2 = WebGL2RenderingContext.prototype.getParameter;
+WebGL2RenderingContext.prototype.getParameter = function(parameter) {
+    if (parameter === 37445) return 'Intel Inc.';
+    if (parameter === 37446) return 'Intel Iris OpenGL Engine';
+    return getParameter2.call(this, parameter);
+};
+const originalQuery = window.navigator.permissions.query;
+window.navigator.permissions.query = (parameters) => (
+    parameters.name === 'notifications' ?
+        Promise.resolve({ state: Notification.permission }) :
+        originalQuery(parameters)
+);
+window.chrome = {
+    runtime: {},
+    loadTimes: function() {},
+    csi: function() {},
+    app: {
+        isInstalled: false,
+        InstallState: { DISABLED: 'disabled', INSTALLED: 'installed', NOT_INSTALLED: 'not_installed' },
+        RunningState: { CANNOT_RUN: 'cannot_run', READY_TO_RUN: 'ready_to_run', RUNNING: 'running' }
+    }
+};
+Object.defineProperty(navigator, 'connection', {
+    get: () => ({ effectiveType: '4g', rtt: 50, downlink: 10, saveData: false })
+});
+const originalToString = Function.prototype.toString;
+Function.prototype.toString = function() {
+    if (this === navigator.permissions.query) {
+        return 'function query() { [native code] }';
+    }
+    return originalToString.call(this);
+};
+"""
