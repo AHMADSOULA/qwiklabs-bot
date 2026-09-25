6f3f3db73e33688883bced3c037eb4cadc8b9cfd
AHMADSOULA <Ahmadmom799@gmail.com>
2026-09-25 21:53:16 +0100

    Update stealth.py

--- automation/stealth.py
+++ automation/stealth.py
@@ -1,13 +1,52 @@
 STEALTH_JS = r"""
 Object.defineProperty(navigator, 'webdriver', { get: () => undefined });
+delete Object.getPrototypeOf(navigator).webdriver;
 Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] });
-Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3, 4, 5] });
 Object.defineProperty(navigator, 'platform', { get: () => 'Win32' });
-window.chrome = { runtime: {}, loadTimes: function(){}, csi: function(){}, app: {} };
+Object.defineProperty(navigator, 'hardwareConcurrency', { get: () => 8 });
+Object.defineProperty(navigator, 'deviceMemory', { get: () => 8 });
+Object.defineProperty(navigator, 'maxTouchPoints', { get: () => 0 });
+Object.defineProperty(navigator, 'vendor', { get: () => 'Google Inc.' });
+Object.defineProperty(navigator, 'plugins', {
+    get: () => {
+        const plugins = [
+            { name: 'Chrome PDF Plugin', filename: 'internal-pdf-viewer' },
+            { name: 'Chrome PDF Viewer', filename: 'mhjfbmdgcfjbbpaeojofohoefgiehjai' },
+            { name: 'Native Client', filename: 'internal-nacl-plugin' },
+        ];
+        plugins.refresh = () => {};
+        plugins.item = (i) => plugins[i];
+        plugins.namedItem = (n) => plugins.find(p => p.name === n);
+        return plugins;
+    }
+});
 const getParameter = WebGLRenderingContext.prototype.getParameter;
 WebGLRenderingContext.prototype.getParameter = function(parameter) {
     if (parameter === 37445) return 'Intel Inc.';
     if (parameter === 37446) return 'Intel Iris OpenGL Engine';
-    return getParameter(parameter);
+    return getParameter.call(this, parameter);
 };
+const originalQuery = window.navigator.permissions.query;
+window.navigator.permissions.query = (parameters) => (
+    parameters.name === 'notifications' ?
+        Promise.resolve({ state: Notification.permission }) :
+        originalQuery(parameters)
+);
+window.chrome = {
+    runtime: {},
+    loadTimes: function() {},
+    csi: function() {},
+    app: { isInstalled: false }
+};
+Object.defineProperty(navigator, 'connection', {
+    get: () => ({ effectiveType: '4g', rtt: 50, downlink: 10 })
+});
+const originalToString = Function.prototype.toString;
+Function.prototype.toString = function() {
+    if (this === navigator.permissions.query) return 'function query() { [native code] }';
+    return originalToString.call(this);
+};
+Object.defineProperty(screen, 'width', { get: () => 1920 });
+Object.defineProperty(screen, 'height', { get: () => 1080 });
+Object.defineProperty(Notification, 'permission', { get: () => 'default' });
 """
